e7c920c91bda8b149d5faf1c88c13255d8163979
water7 <m_tomoki_0724@outlook.jp>
2026-05-04 03:17:59 +0900

    Added has_cycle_topological_sort.cpp to determine if a graph contains cycles.

---
+++ has_cycle_topological_sort.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+using vi = vector<int>;
+using vvi = vector<vi>;
+
+
+/**
+ * グラフが閉路を持つかどうかトポロジカルソートで判定する
+ * 計算量はO(|V|+|E|)
+ * @param graph グラフを表す隣接リスト
+ * @return グラフが閉路を持つならtrueを返す
+ */
+bool has_cycle(const vvi& graph) {
+    int n = graph.size();
+    vi in_degree(n);
+    for (int i = 0; i < n; ++i) {
+        for (int j : graph[i]) {
+            in_degree[j]++;
+        }
+    }
+    
+    queue<int> que;
+    for (int i = 0; i < n; ++i) {
+        if (in_degree[i] == 0) que.push(i);
+    }
+    
+    int visited_cnt = 0;
+    while (! que.empty()) {
+        int st = que.front(); que.pop();
+        ++visited_cnt;
+        for (int ds : graph[st]) {
+            --in_degree[ds];
+            if (in_degree[ds] == 0) {
+                que.push(ds);
+            }
+        }
+    }
+    
+    // 削除できたノード数が全体より少なければ，閉路が存在する．
+    return visited_cnt != n;
+}
