a3df777a6b8c89e766aedfe8b52866d5c7db3939
water7 <m_tomoki_0724@outlook.jp>
2026-05-04 03:28:49 +0900

    Added documentation comments to pow.cpp.

--- pow.cpp
+++ pow.cpp
@@ -2,6 +2,14 @@
 using namespace std;
 using ll = long long;
 
+/**
+ * X^Y % MOD を計算する
+ * 計算量はO(log Y)
+ * @param x int 底の値
+ * @param y int 指数の値
+ * @param mod int 法の値
+ * @return X^Y % MOD の値
+ */
 int pow(int x, int y, int mod) {
   int res = 1;
   for (; y > 0; y >>= 1) {
@@ -11,6 +19,14 @@ int pow(int x, int y, int mod) {
   return res;
 }
 
+/**
+ * X^Y を計算する
+ * long longのオーバーロード
+ * 計算量はO(log Y)
+ * @param x ll 底の値
+ * @param y ll 指数の値
+ * @return X^Y の値
+ */
 ll pow(ll x, ll y) {
   ll res = 1;
   for (; y > 0; y >>= 1) {
@@ -20,6 +36,14 @@ ll pow(ll x, ll y) {
   return res;
 }
 
+/**
+ * X^Y % MOD を計算する
+ * long longのオーバーロード
+ * 計算量はO(log Y)
+ * @param x ll 底の値
+ * @param y ll 指数の値
+ * @return X^Y % MOD の値
+ */
 ll pow(ll x, ll y, ll mod) {
   ll res = 1;
   for (; y > 0; y >>= 1) {
