d2b5eca10e71c8525c693e6b4cd536bc084699ea
water7 <m_tomoki_0724@outlook.jp>
2026-05-05 02:06:09 +0900

    Fixed to avoid using aliases.

--- pow.cpp
+++ pow.cpp
@@ -1,13 +1,11 @@
 #include <bits/stdc++.h>
-using namespace std;
-using ll = long long;
 
 /**
  * X^Y % MOD を計算する
  * 計算量はO(log Y)
- * @param x int 底の値
- * @param y int 指数の値
- * @param mod int 法の値
+ * @param x 底の値
+ * @param y 指数の値
+ * @param mod 法の値
  * @return X^Y % MOD の値
  */
 int pow(int x, int y, int mod) {
@@ -23,12 +21,12 @@ int pow(int x, int y, int mod) {
  * X^Y を計算する
  * long longのオーバーロード
  * 計算量はO(log Y)
- * @param x ll 底の値
- * @param y ll 指数の値
+ * @param x 底の値
+ * @param y 指数の値
  * @return X^Y の値
  */
-ll pow(ll x, ll y) {
-  ll res = 1;
+long long pow(long long x, long long y) {
+  long long res = 1;
   for (; y > 0; y >>= 1) {
     if (y & 1) res *= x;
     x *= x;
@@ -40,12 +38,12 @@ ll pow(ll x, ll y) {
  * X^Y % MOD を計算する
  * long longのオーバーロード
  * 計算量はO(log Y)
- * @param x ll 底の値
- * @param y ll 指数の値
+ * @param x 底の値
+ * @param y 指数の値
  * @return X^Y % MOD の値
  */
-ll pow(ll x, ll y, ll mod) {
-  ll res = 1;
+long long pow(long long x, long long y, long long mod) {
+  long long res = 1;
   for (; y > 0; y >>= 1) {
     if (y & 1) res = res * x % mod;
     x = x * x % mod;
